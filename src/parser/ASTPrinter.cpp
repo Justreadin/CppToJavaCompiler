3be37e90c8c7439bec51ae065049ecd8b824c9db
Favour <dave.400g@gmail.com>
2025-03-05 06:06:04 +0100

    Added the utils

--- src/parser/ASTPrinter.cpp
+++ src/parser/ASTPrinter.cpp
@@ -1,5 +1,5 @@
 #include "ASTPrinter.h"
-#include "ASTNode.h"  // Ensure ASTNode.h is included
+#include "ASTNode.h"
 #include <iostream>
 #include <memory>
 
@@ -18,45 +18,92 @@ void ASTPrinter::print(const std::shared_ptr<ASTNode>& node, int indent) {
     printIndent(indent);
     std::cout << "└── " << node->toString() << std::endl;
 
-    // Handle AST node types
-    if (auto binaryExpr = std::dynamic_pointer_cast<BinaryExpressionNode>(node)) {
-        print(binaryExpr->left, indent + 4);
-        print(binaryExpr->right, indent + 4);
-    }
-    else if (auto funcCall = std::dynamic_pointer_cast<FunctionCallNode>(node)) {
-        print(funcCall->functionName, indent + 4);
-        for (const auto& arg : funcCall->arguments) {
-            print(arg, indent + 8);
+    switch (node->type) {
+        case NodeType::BINARY_EXPRESSION: {
+            auto binaryExpr = std::static_pointer_cast<BinaryExpressionNode>(node);
+            print(binaryExpr->left, indent + 4);
+            printIndent(indent + 2);
+            std::cout << "Operator: " << binaryExpr->op << std::endl;
+            print(binaryExpr->right, indent + 4);
+            break;
         }
-    }
-    else if (auto varDecl = std::dynamic_pointer_cast<VariableDeclarationNode>(node)) {
-        print(varDecl->identifier, indent + 4);
-        print(varDecl->initializer, indent + 4);
-    }
-    else if (auto funcDecl = std::dynamic_pointer_cast<FunctionDeclarationNode>(node)) {
-        print(funcDecl->functionName, indent + 4);
-        for (const auto& param : funcDecl->parameters) {
-            print(param, indent + 8);
+        case NodeType::FUNCTION_CALL: {
+            auto funcCall = std::static_pointer_cast<FunctionCallNode>(node);
+            print(funcCall->functionName, indent + 4);
+            for (const auto& arg : funcCall->arguments) {
+                print(arg, indent + 8);
+            }
+            break;
         }
-        print(funcDecl->body, indent + 8);
-    }
-    else if (auto ifStmt = std::dynamic_pointer_cast<IfStatementNode>(node)) {
-        print(ifStmt->condition, indent + 4);
-        print(ifStmt->thenBlock, indent + 4);
-        if (ifStmt->elseBlock) {
-            print(ifStmt->elseBlock, indent + 4);
+        case NodeType::VARIABLE_DECLARATION: {
+            auto varDecl = std::static_pointer_cast<VariableDeclarationNode>(node);
+            printIndent(indent + 4);
+            std::cout << "Type: " << varDecl->type << std::endl;
+            print(varDecl->identifier, indent + 4);
+            if (varDecl->initializer) {
+                print(varDecl->initializer, indent + 4);
+            }
+            break;
         }
-    }
-    else if (auto whileLoop = std::dynamic_pointer_cast<WhileLoopNode>(node)) {
-        print(whileLoop->condition, indent + 4);
-        print(whileLoop->body, indent + 4);
-    }
-    else if (auto block = std::dynamic_pointer_cast<BlockNode>(node)) {
-        for (const auto& stmt : block->statements) {
-            print(stmt, indent + 8);
+        case NodeType::FUNCTION_DECLARATION: {
+            auto funcDecl = std::static_pointer_cast<FunctionDeclarationNode>(node);
+            printIndent(indent + 4);
+            std::cout << "Return Type: " << funcDecl->returnType << std::endl;
+            print(funcDecl->functionName, indent + 4);
+            for (const auto& param : funcDecl->parameters) {
+                print(param, indent + 8);
+            }
+            print(funcDecl->body, indent + 8);
+            break;
         }
-    }
-    else if (auto returnStmt = std::dynamic_pointer_cast<ReturnStatementNode>(node)) {
-        print(returnStmt->expression, indent + 4);
+        case NodeType::RETURN_STATEMENT: {
+            auto returnStmt = std::static_pointer_cast<ReturnStatementNode>(node);
+            print(returnStmt->expression, indent + 4);
+            break;
+        }
+        case NodeType::IF_STATEMENT: {
+            auto ifStmt = std::static_pointer_cast<IfStatementNode>(node);
+            print(ifStmt->condition, indent + 4);
+            print(ifStmt->thenBlock, indent + 4);
+            if (ifStmt->elseBlock) {
+                print(ifStmt->elseBlock, indent + 4);
+            }
+            break;
+        }
+        case NodeType::WHILE_LOOP: {
+            auto whileLoop = std::static_pointer_cast<WhileLoopNode>(node);
+            print(whileLoop->condition, indent + 4);
+            print(whileLoop->body, indent + 4);
+            break;
+        }
+        case NodeType::BLOCK: {
+            auto block = std::static_pointer_cast<BlockNode>(node);
+            for (const auto& stmt : block->statements) {
+                print(stmt, indent + 8);
+            }
+            break;
+        }
+        case NodeType::IDENTIFIER: {
+            auto identifier = std::static_pointer_cast<IdentifierNode>(node);
+            printIndent(indent + 4);
+            std::cout << "Identifier: " << identifier->name << std::endl;
+            break;
+        }
+        case NodeType::NUMBER_LITERAL: {
+            auto number = std::static_pointer_cast<NumberNode>(node);
+            printIndent(indent + 4);
+            std::cout << "Number: " << number->value << std::endl;
+            break;
+        }
+        case NodeType::STRING_LITERAL: {
+            auto strNode = std::static_pointer_cast<StringNode>(node);
+            printIndent(indent + 4);
+            std::cout << "String: \"" << strNode->value << "\"" << std::endl;
+            break;
+        }
+        default:
+            printIndent(indent + 4);
+            std::cout << "(Unknown node type)" << std::endl;
+            break;
     }
 }
